0d191c66dc5b073da127bd9c9627c567542eed1f
simenravnik <ravnik.simen@gmail.com>
2019-10-25 13:28:09 +0200

    Add developed K-Medoids algorithm
    Plus included some test files

--- naloga2.py
+++ naloga2.py
@@ -1,10 +1,10 @@
+
 import numpy as np
 
 
-def read_file(files):
+def read_file(files, n=3):
     # texts is a dict of dicts, so that keys are languages, and values is a dict
     # of unique n-consecutive strings in language and their number of repetitions
-    n = 3
     texts = {}
     for i in files:
         f = open(i, "rt", encoding="utf8").read().\
@@ -27,70 +27,112 @@ def read_file(files):
                 unique.update({n_consecutive: 1})
         # we put the count of unique n-consecutive strings in text dict
         texts.update({i: unique})
-
     return texts
 
 
 class KMedoidsClustering:
     def __init__(self, data):
         self.data = data
-        self.unique = self.get_unique()
-
-    def get_unique(self):
-        unique_elements = []
-        for l in self.data:
-            # unique n consecutive characters of lists
-            unique_elements = list(set().union(unique_elements, l))
-        return unique_elements
-
-    def count_frequency(self, list1, list2):
-        vector1 = []
-        vector2 = []
-        # counting number of times element appears in list1 and list2
-        for element in self.unique:
-            count_list1 = list1.count(element)
-            count_list2 = list2.count(element)
-            if count_list1 != 0 or count_list2 != 0:
-                vector1.append(count_list1)
-                vector2.append(count_list2)
-
-        return vector1, vector2
 
     @staticmethod
-    def cosine_similarity(vector1, vector2):
-        # convert lists to np.array
-        vector1 = np.array(vector1)
-        vector2 = np.array(vector2)
+    def cosine_similarity(language1, language2):
+        # calculating cosine similarity between two languages
 
-        # calculating cosine distance between two vectors
-        dot = np.dot(vector1, vector2)  # dot product
+        # intersection of the same n-consecutive strings for dot product
+        # common elements (keys) of both dicts
+        common = set(language1.keys()) & set(language2.keys())
+        # initialize dot product
+        dot = 0
+        for i in common:
+            dot += (language1.get(i) * language2.get(i))
+
+        # calculating the magnitude of vectors
+        vector1 = np.array(list(language1.values()))
+        vector2 = np.array(list(language2.values()))
         norm_vector1 = np.sqrt(np.sum(vector1 ** 2))  # square root of sum of square element in values of vector1
         norm_vector2 = np.sqrt(np.sum(vector2 ** 2))  # square root of sum of square element in values of vector2
+        # cosine similarity
         cos = dot / (norm_vector1 * norm_vector2)
-
         return cos
 
-    def k_means(self):
-        for i in self.data:
-            for j in self.data:
-                if i != j:
-                    v1, v2 = self.count_frequency(i, j)
-                    dist = self.cosine_similarity(v1, v2)
-                    print(dist)
+    def k_medoids(self):
+        # choose k random languages
+        # sort of: language1, values1 = random.choice(list(self.data.keys()))
+        # for now, lets choose manually
+
+        leader1 = "test/slo.txt"
+        leader2 = "test/ang.txt"
+
+        groups = {leader1: set(), leader2: set()}
+        old_groups = {}
+        # while loop until the groups remain the same
+        while groups.keys() != old_groups.keys():
+            # save current groups to old_groups for comparison in while loop
+            old_groups = groups
+
+            # ----------- CREATING NEW GROUPS BECAUSE LEADERS MIGHT CHANGE ----------- #
+            # iterate over all languages and see which leader it belongs
+            for i in self.data:
+                v = self.data.get(i)    # values of current language
+                max_similarity = -1
+                belong_to = "none"  # currently belong to nobody
+                # iterate over leaders and calculating cosine distance between current language and leader language
+                for leader in groups.keys():
+                    values = self.data.get(leader)
+                    similarity = self.cosine_similarity(values, v)  # cosine similarity
+                    if max_similarity < similarity:
+                        max_similarity = similarity
+                        belong_to = leader
+                # update current language (i) to leader who is most similar (belong_to)
+                groups[belong_to].update([i])
+
+            # ----- RECALCULATING WHO ARE THE LEADERS BECAUSE GROUPS MIGHT HAVE CHANGED ----- #
+            new_groups = {}  # new dict because we cannot change one (groups) in loop
+            # iterate over all groups
+            for leader in groups.keys():
+                group = groups.get(leader)
+                dist = 0
+                min_dist = -1
+                new_leader = "None"
+                # iterate over all elements in group and calculating
+                # the distance from all elements to all other elements
+                for i in group:
+                    values_i = self.data.get(i)
+                    for j in group:
+                        if i != j:
+                            values_j = self.data.get(j)
+                            dist += (1 - self.cosine_similarity(values_i, values_j))    # cosine distance
+                    # choosing min distance from element to all other elements as the new leader
+                    if min_dist == -1 or dist < min_dist:
+                        min_dist = dist
+                        new_leader = i
+                    dist = 0
+                # setting current group new leader
+                new_groups[new_leader] = group
+            groups = new_groups
+        print(groups)
 
     def run(self):
-        self.k_means()
+        self.k_medoids()
 
 
 if __name__ == "__main__":
     DATA_FILE1 = "src3.txt"
     DATA_FILE2 = "slv.txt"
     DATA_FILE3 = "slo.txt"
-    TEST = "test.txt"
-    DATA_FILES = [DATA_FILE1, DATA_FILE2]
 
-    file = read_file(DATA_FILES)
+    # test files
+    TEST1 = "test/slo.txt"
+    TEST2 = "test/svk.txt"
+    TEST3 = "test/hrv.txt"
+    TEST4 = "test/ang.txt"
+    TEST5 = "test/nem.txt"
+    TEST6 = "test/pol.txt"
+    TEST7 = "test/spn.txt"
+    TEST8 = "test/prt.txt"
+    TEST9 = "test/itl.txt"
+    DATA_FILES = [TEST1, TEST2, TEST3, TEST4, TEST5, TEST6, TEST7, TEST8, TEST9]
 
-    # KMC = KMedoidsClustering(read_file(DATA_FILES))
-    # KMC.run()
+    KMC = KMedoidsClustering(read_file(DATA_FILES))
+    KMC.run()
 
